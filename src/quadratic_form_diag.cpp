4fe7d0e82bfdcfd3de8fba333c6ce05ca5b650ef
borishejblum <boris.hejblum@gmail.com>
2025-03-11 13:22:15 +0100

    removing wrong comments

--- src/quadratic_form_diag.cpp
+++ src/quadratic_form_diag.cpp
@@ -1,8 +1,7 @@
 #include <RcppEigen.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
+// You can source this function into an R session using the Rcpp::sourceCpp
 // function (or via the Source button on the editor toolbar). Learn
 // more about Rcpp at:
 //
