2fb2649ff0f3491dc4cb2fcf0afc947fec60bfbd
Annesh Pal <anneshpal@gmail.com>
2025-04-03 12:58:56 +0200

    openmp activation

--- src/sweep_3D.cpp
+++ src/sweep_3D.cpp
@@ -29,7 +29,12 @@ NumericVector sweep_3D(NumericVector A, NumericVector R, IntegerVector dims, int
   Map<VectorXd> vec_result(Rcpp::as<Map<VectorXd>>(result));
 
   // Parallelize over T0 slices
-#pragma omp parallel for num_threads(n_threads)
+## -- compiling for OpenMP 
+  PKG_CXXFLAGS=-fopenmp
+##
+## -- linking for OpenMP
+  PKG_LIBS= -fopenmp -lgomp 
+  #pragma omp parallel for num_threads(n_threads)
   for (int t = 0; t < T0; t++) {
     int offset = t * D2;  // Start of slice t in flat memory
     vec_result.segment(offset, D2) *= vec_R[t];  // Scale the slice
