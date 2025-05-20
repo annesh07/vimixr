12c48b897f04cf3d6229de906a1c8e473564f8d9
borishejblum <boris.hejblum@gmail.com>
2025-05-20 14:27:27 +0200

    maintainer email update + Rmarkdown roxygen

--- R/cvi_npmm.R
+++ R/cvi_npmm.R
@@ -168,21 +168,16 @@
 #' distributed ('off-diagonal normal')
 #' @param ... additional parameters, further details given below
 #'
-#' @returns The following objects are returned by \code{vimixr} \describe{
-#'  \item{\code{alpha}: posterior DP concentration parameter}{}
-#'  \item{\code{Cluster number}: number of clusters from
-#'        posterior probability allocation matrix}{}
-#'  \item{\code{Cluster Proportion}: cluster proportions from
-#'        posterior probability allocation matrix}{}
-#'  \item{\code{log Probability matrix}: log of posterior probability allocation
-#'        matrix}{}
-#'  \item{\code{ELBO}: Optimisation of the ELBO function}{}
-#'  \item{\code{Iterations}: Number of iterations required for convergence}{}
-#'  \item{\code{PCA_viz}: A PCA \code{\link{ggplot2}} plot to visualize the clustering
-#'        of data based on cluster labels}{}
-#'  \item{\code{ELBO_viz}: A line \code{\link{ggplot2}} plot to visualize the ELBO
-#'        optimisation}{}
-#' }
+#' @returns `[vimixr()]` returns a `list` with the following elements:
+#'   - `alpha`: posterior DP concentration parameter
+#'   - `Cluster number`: number of clusters from posterior probability allocation matrix
+#'   - `Cluster Proportion`: cluster proportions from posterior probability allocation matrix
+#'   - `log Probability matrix`: log of posterior probability allocation matrix
+#'   - `ELBO`: Optimisation of the ELBO function
+#'   - `Iterations`: Number of iterations required for convergence
+#'   - `PCA_viz`: A PCA `[ggplot2]` plot to visualize the clustering of data based on cluster labels
+#'   - `ELBO_viz`: A line `[ggplot2]` plot to visualize the ELBO optimisation
+#'
 #'
 #' @importFrom Rfast rowsums colsums spdinv eachrow eachcol.apply Diag.fill
 #' Diag.matrix
@@ -228,7 +223,7 @@ cvi_npmm <- function(X, variational_params,
   N <- nrow(X)
   D <- ncol(X)
   T0 <- variational_params
-  log_prob_matrix <- log_prob_matrix[, order(Rfast::colsums(exp(log_prob_matrix)), 
+  log_prob_matrix <- log_prob_matrix[, order(Rfast::colsums(exp(log_prob_matrix)),
                                            decreasing = TRUE)]
   varargs <- list(...)
   params <- list()
@@ -248,7 +243,7 @@ cvi_npmm <- function(X, variational_params,
   params$log_prob_matrix <- log_prob_matrix
   params$P <- t(apply(exp(log_prob_matrix), 1, function(x){x/sum(x)}))
   RP <- Rfast::colsums(params$P)
-  
+
 
   #updating the parameter list based on the conditions
   if(covariance_type == "diagonal") {
@@ -421,7 +416,7 @@ cvi_npmm <- function(X, variational_params,
                        "PC2" = pca$x[,2],
                        "Cluster" = as.factor(clustering))
   ggplot_pca <- ggplot2::ggplot(pca_df, ggplot2::aes(x = .data$PC1, y = .data$PC2,
-                                                       color = .data$Cluster, 
+                                                       color = .data$Cluster,
                                                      shape = .data$Cluster)) +
     ggplot2::geom_point(size = 3, alpha = 0.8) +
     ggplot2::labs(title = "PCA projection of clustered data", x = "PC 1", y = "PC 2") +
@@ -435,38 +430,38 @@ cvi_npmm <- function(X, variational_params,
     ggplot2::geom_line() +
     ggplot2::labs(title = "ELBO Optimisation", x = "Iterations", y = "ELBO") +
     ggplot2::theme_minimal()
-  
+
   post_distribution = list()
-  L1 <- params$post_mean_eta 
-  
+  L1 <- params$post_mean_eta
+
   if(covariance_type == "diagonal") {
-    
+
     if(fixed_variance) {
-      L2 <- params$post_precision_scalar_eta 
+      L2 <- params$post_precision_scalar_eta
 
       post_distribution[["Mean"]] = L1/c(L2)
-      
+
     } else {
-      G1 <- params$post_shape_scalar_cov 
-      G2 <- params$post_rate_scalar_cov 
+      G1 <- params$post_shape_scalar_cov
+      G2 <- params$post_rate_scalar_cov
       L2 <- params$post_precision_scalar_eta <- varargs$post_precision_scalar_eta
-      
+
       post_distribution[["Mean"]] = L1/c(L2)
       post_distribution[["Precision"]] = (G1/G2)*diag(D)
       #G2/G1 instead of G1/G2 because prior on the precision diagonal scalar
     }
-    
+
   } else if(covariance_type == "full") {
-    
+
     if(fixed_variance) {
       L2 <- params$post_cov_eta
       L21 <- matrix(0, nrow = T0, ncol = D)
       for (i in 1:T0){
         L21[i,] = mat_mult(L1[i,, drop = FALSE], L2[,,i])
       }
-      
-      post_distribution[["Mean"]] = L21  
-      
+
+      post_distribution[["Mean"]] = L21
+
     } else {
       if(!cluster_specific_covariance) {
         if(variance_prior_type == "IW"){
@@ -478,17 +473,17 @@ cvi_npmm <- function(X, variational_params,
           for (i in 1:T0){
             L21[i,] = mat_mult(L1[i,, drop = FALSE], L2[,,i])
           }
-          
+
           post_distribution[["Mean"]] = L21
           post_distribution[["Precision"]] = nu*V
-          
+
         } else if (variance_prior_type == "decomposed"){
           a1 <- params$post_shape_diag_decomp
           b1 <- params$post_rate_diag_decomp
           mu1 <- params$post_mean_offdiag_decomp
           c1 <- params$post_var_offdiag_decomp
           L2 <- params$post_cov_eta
-          
+
           mean_lower <- matrix(0, nrow = D, ncol = D) #mean matrix of the decomposed
           mean_lower[lower.tri(mean_lower, diag = FALSE)] <- mu1
           sigma_lower <- matrix(0, nrow = D, ncol = D) #var matrix of the decomposed
@@ -498,20 +493,20 @@ cvi_npmm <- function(X, variational_params,
           #expected inverse of C0; covariance matrix of data
           inv_C0 <- mat_mult(mean_L, t(mean_L)) +
             diag(Rfast::rowsums(sigma_lower))
-          
+
           L21 <- matrix(0, nrow = T0, ncol = D)
           for (i in 1:T0){
             L21[i,] = mat_mult(L1[i,, drop = FALSE], L2[,,i])
           }
-          
+
           post_distribution[["Mean"]] = L21
           post_distribution[["Precision"]] = inv_C0
-          
+
         } else {
           stop("'variance_prior_type' can only be either 'IW' or 'decomposed'
                when 'cluster_specific_covariance' is FALSE")
         }
-        
+
       }else{
         if(variance_prior_type == "IW"){
           nu1 <- params$post_df_cs_cov
@@ -520,10 +515,10 @@ cvi_npmm <- function(X, variational_params,
           V1_inv <- array(apply(V1, 3, function(x){spdinv(x)}), dim = dim(V1))
           #expectation of inverse of data covariance matrix
           inv_C0 <- sweep_3D(V1_inv, nu1, c(D, D, T0))
-          
+
           post_distribution[["Mean"]] = L1
           post_distribution[["Precision_cs"]] = inv_C0
-          
+
         } else if (variance_prior_type == "sparse"){
           a1 <- params$post_shape_d_cs_cov
           B1 <- params$post_rate_d_cs_cov
@@ -533,35 +528,35 @@ cvi_npmm <- function(X, variational_params,
           for (i in 1:T0){
             inv_C0[,,i] <- temp <- Rfast::Diag.matrix(D, a1[1,i]/B1[i,])
           }
-          
+
           post_distribution[["Mean"]] = L1
           post_distribution[["Precision_cs"]] = inv_C0
-          
+
         } else if (variance_prior_type == "off-diagonal normal"){
           a1 <- params$post_shape_d_cs_cov
           B1 <- params$post_rate_d_cs_cov
           C1 <- params$post_mean_offd_cs_cov
-          
+
           #expectation of inverse of data covariance matrix
           inv_C0 <- array(0, c(D, D, T0))
           for (i in 1:T0){
             inv_C0[,,i] <- temp <- Rfast::Diag.fill(C1[,,i], a1[1,i]/B1[i,])}
-          
+
           post_distribution[["Mean"]] = L1
           post_distribution[["Precision_cs"]] = inv_C0
-          
+
         } else {
           stop("'variance_prior_type' can only be either 'IW' or 'decomposed'
                when 'cluster_specific_covariance' is TRUE")
         }
       }
-      
+
     }
   } else {
     stop("covariance_type can only be either 'diagonal' or 'full'.")
   }
-  
-  
+
+
   posterior <- c(list("alpha" = alpha0, "Cluster number" = clustnum,
                     "Cluster Proportion" = clust,
                     "log Probability matrix" = Plog), post_distribution)
